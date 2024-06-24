4be41ef91f85a6674a9384d06f820d20e13ac474
sarahaglenn <sar00@byui.edu>
2024-06-24 22:44:53 +0200

    add weather info to course home page

--- scripts/home.js
+++ scripts/home.js
@@ -120,3 +120,39 @@ generateCourseCards(courses);
 credits.innerHTML = `Total credits required: ${courses.reduce(function (acc, course) {
     return acc + course.credits;
 }, 0)}`;
+
+// WEATHER CARD
+const temp = document.getElementById('current-temp');
+const weatherIcon = document.getElementById('weather-icon');
+const figcaption = document.getElementById('weather-caption');
+
+const lat = "64.75";
+const long = "20.95";
+const key = "2026cc9883490e04a8412142d4da0c5d";
+const url = `//api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&units=metric&appid=${key}`;
+
+async function apiFetch() {
+    try {
+        const response = await fetch(url);
+        if (response.ok) {
+            const data = await response.json();
+            displayResults(data);
+        } else {
+            throw Error(await response.text());
+        }
+    }
+    catch(error) {
+        console.log(error);
+    }
+}
+
+function displayResults(data) {
+    const roundedTemp = Math.round(data.main.temp);
+    temp.textContent = `${roundedTemp}°C`;
+    const iconSRC = `//openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
+    let alt = data.weather[0].description;
+    weatherIcon.setAttribute('src', iconSRC);
+    weatherIcon.setAttribute('alt', alt);
+    figcaption.textContent = `${alt}`;
+}
+apiFetch()
